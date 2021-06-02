d613930c98a62fc351e2716a96edb3d2e23631cd
Rami Sabbagh <ramilego4game@gmail.com>
2021-06-02 20:13:25 +0300

    (some forgotten stashed code)

--- Toolkit/Assembler/errorMessages.lua
+++ Toolkit/Assembler/errorMessages.lua
@@ -7,6 +7,24 @@ local errors = {
     ["invalid-label-name"] = "Label name can be only made of letters, digits and underscores, and cannot start with a digit."
 }
 
+local e2 = {
+    ["invalid-scoping-line"] = "Invalid scoping line, the line should only contain { or } and an optional comment.",
+    ["invalid-instruction-name"] = "Invalid instruction name, the instruction name can only contain letters.",
+    ["extra-operands"] = "Instructions can't accept more than 2 operands.",
+    ["invalid-register-name"] = "operand #%d: Invalid register name, the register name can only contain letters.",
+    ["invalid-hex-value"] = "operand #%d: Invalid hexadecimal value.",
+    ["invalid-bin-value"] = "operand #%d: Invalid binary value.",
+    ["invalid-dec-value"] = "operand #%d: Invalid decimal value.",
+    ["label-operand-not-closed"] = "operand #%d: Label operand missing closing ')'.",
+    ["missing-label-name"] = "operand #%d: Missing label name.",
+    ["invalid-label-name"] = "operand #%d: Invalid label name, it can be only made of letters, digits, dollar signs and underscores, and cannot start with a digit.",
+    ["memref-operand-not-closed"] = "operand #%d: Memory reference operand missing closing ']'.",
+
+    ["invalid-operand"] = "operand #%d: Invalid operand.",
+
+    ["wip"] = "The assembler is still work in progress!",
+}
+
 local exitcodes = {}
 
 for k,_ in pairs(errors) do
--- Toolkit/Assembler/init.lua
+++ Toolkit/Assembler/init.lua
@@ -2,6 +2,129 @@
 
 local assembler = {}
 
+--Parse a line of assembly code
+function assembler:parseLine(line)
+    --Clear leading whitespace
+    line = line:gsub("^%s*", "")
+
+    --Clear leading newline characters
+    line = line:gsub("[\r\n]*$", "")
+
+    if line:sub(1,1) == "#" then return self:parseDirective(line) end
+    if line:sub(1,1) == "." then return self:parseMacro(line) end
+    return self:parseInstruction(line)
+end
+
+--Parse an assembler directive line
+function assembler:parseDirective(line)
+    --TODO: Wrote directives parser
+    return "invalid", "wip"
+end
+
+--Parse an assember macro line
+function assembler:parseMacro(line)
+    --TODO: Write macros parser
+    return "invalid", "wip"
+end
+
+--Parse an instruction line
+function assembler:parseInstruction(line)
+    --Clear comments and leading whitespace from the line
+    line = line:gsub("%s*;.*$", "")
+
+    --Empty line
+    if #line == 0 then return "empty" end
+
+    --Scoping line
+    if line:sub(1,1) == "{" or line:sub(1,1) == "}" then
+        if #line > 1 then return "invalid", "invalid-scoping-line" end
+
+        if line == "{" then return "scope", "open"
+        else return "scope", "close" end
+    end
+
+    --Instruction name
+    local instructionName = line:match("[^%s,]*"):lower()
+    if instructionName:match("%A") then return "invalid", "invalid-instruction-name" end
+    line = line:sub(#instructionName+1, -1) --Remove the instruction name from the line
+
+    local nextOperand = line:gmatch("%s*[,%s]%s*[^,%s]*")
+
+    local operand1, operand2 = nextOperand(), nextOperand()
+    if operand1 and operand2 and nextOperand() then return "invalid", "extra-operands" end
+
+    if operand1 then
+        operand1 = {self:parseOperand(operand1)}
+        if operand1[1] == "invalid" then return "invalid", operand1[2], 1, unpack(operand1, 3) end
+    end
+
+    if operand2 then
+        operand2 = {self:parseOperand(operand2)}
+        if operand2[1] == "invalid" then return "invalid", operand2[2], 2, unpack(operand2, 3) end
+    end
+
+    return "instruction", instructionName, operand1, operand2
+end
+
+function assembler:parseOperand(operand)
+    --Clear the leading ',' and whitespace
+    operand = operand:gsub("^%s*[,%s]%s*", "")
+
+    --Register operand
+    if operand:match("^%a") then
+        if operand:match("%A") then return "invalid", "invalid-register-name" end
+        return "register", operand
+    end
+
+    --Literal number value
+    if operand:match("^%d") then
+        --Hexademical number
+        if operand:match("0[xX]") then
+            operand = tonumber(operand, 16)
+            if not operand then return "invalid", "invalid-hex-value" end
+            return "literal", operand
+        end
+
+        --Binary number
+        if operand:match("0[bB]") then
+            operand = tonumber(operand, 2)
+            if not operand then return "invalid", "invalid-bin-value" end
+            return "literal", operand
+        end
+
+        --Decimal number
+        operand = tonumber(operand, 10)
+        if not operand then return "invalid", "invalid-dec-value" end
+        return "literal", operand
+    end
+
+    --Label operand
+    if operand:sub(1,1) == "(" then
+        if operand:sub(-1,-1) ~= ")" then return "invalid", "label-operand-not-closed" end
+        --Clear () and the whitespace between them
+        operand = operand:gsub("^%(%s*"):gsub("%s*%)$")
+        --Missing label name
+        if #operand == 0 then return "invalid", "missing-label-name" end
+        --Match a label name, must give a nil if it's an invalid name
+        operand = operand:match("^[_%$%a][_%$%a%d]*$")
+        if not operand then return "invalid", "invalid-label-name" end
+
+        return "label", operand:lower()
+    end
+
+    --Memory reference
+    if operand:sub(1,1) == "[" then
+        if operand:sub(-1,-1) ~= "]" then return "invalid", "memref-operand-not-closed" end
+
+        --TODO: Write the missing code here.
+        return "invalid", "wip"
+    end
+
+    --Invalid operand, didn't fit any known type
+    return "invalid", "invalid-operand"
+end
+
+
 function assembler:parseLine(line)
     line:gsub("^%s*", "") --Clear leading whitespace
 
